520f4ebffc955f406b9f9cccbc78e78101cb4a79
kaspiotr <kaspiotr@gmail.com>
2018-07-21 13:50:09 +0200

    Add realising memory to all activities

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -13,6 +13,18 @@ public class PhrasesActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
 
+    /**
+     * This listener gets triggered when the {@link MediaPlayer} has completed
+     * playing the audio file.
+     */
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            // Now that the sound file has finished playing, release the media player resources.
+            releaseMediaPlayer();
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +34,7 @@ public class PhrasesActivity extends AppCompatActivity {
 
         words.add(new Word("Where are you going?", "minto wuksus", R.raw.phrase_where_are_you_going));
         words.add(new Word("What is your name?", "tinne oyaase'ne", R.raw.phrase_what_is_your_name));
-        words.add(new Word("My name is...", "oyaaset...", R.raw.phrase_how_are_you_feeling));
+        words.add(new Word("My name is...", "oyaaset...", R.raw.phrase_my_name_is));
         words.add(new Word("How are you feeling?", "michekses", R.raw.phrase_how_are_you_feeling));
         words.add(new Word("I'm feeling good.", "kuchi achit", R.raw.phrase_im_feeling_good));
         words.add(new Word("Are you coming?", "eenes'aa?", R.raw.phrase_are_you_coming));
@@ -43,8 +55,28 @@ public class PhrasesActivity extends AppCompatActivity {
                 Word word = words.get(i);
                 mediaPlayer = MediaPlayer.create(PhrasesActivity.this, word.getmAudioResourceId());
                 mediaPlayer.start();
+
+                mediaPlayer.setOnCompletionListener(mCompletionListener);
             }
         });
 
+
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mediaPlayer = null;
+        }
     }
 }
